fdb244b645738befc03a6d2bcb9b5ae17b54fd2b
Siddharth Shah <siddharthshah23@outlook.com>
2021-02-25 20:49:45 -0500

    Added validation

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const stripe = require("stripe")(
+  "sk_test_51IOB8xHr9xsmMfAiaPK89B4zEq6Hftyl6WMDE2XXsRUb6bFHqMlNQF65eLZi5CIDoReBh6aejWoM6dU4PZIA6iF200sHOGcAQd"
+);
 
 const app = express();
+
+// Connect Databse
+
+connectDB();
+
+// Init Middleware
+
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send({ msg: "Welecome to ContactKeeper API" }));
 
@@ -9,6 +21,16 @@ app.get("/", (req, res) => res.send({ msg: "Welecome to ContactKeeper API" }));
 app.use("/api/users", require("./server/routes/users"));
 app.use("/api/auth", require("./server/routes/auth"));
 app.use("/api/contact", require("./server/routes/contact"));
+
+app.post("/api/test", (req, res) => {
+  stripe.customers
+    .create({
+      email: "test@test3.ca",
+    })
+    // .then(res.send("succesfull"))
+    .then((customer) => res.send(customer.id))
+    .catch((error) => console.log(error));
+});
 
 const PORT = process.env.PORT || 5000;
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,27 @@
 const express = require("express");
-
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
 // @router  POST api/user
 //@desc     REGISTER A USER
 //@access   PUBLIC
-router.post("/", (req, res) => {
-  res.send({ msg: "Register a user" });
-});
+router.post(
+  "/",
+  [
+    check("name", "name is required").notEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with minimum 8 character"
+    ).isLength({ min: 8 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("passed");
+  }
+);
 
 module.exports = router;
